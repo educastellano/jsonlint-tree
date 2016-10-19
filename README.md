d62401a6ad05b1bcfac8febc7ff7190873aef955
Eduard Castellano <educastellano08@gmail.com>
2016-10-19 14:05:23 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# jsonlint-tree
+
+[jsonlint](https://www.npmjs.com/package/jsonlint) a directory recursively.
+
+## Install
+
+    npm install jsonlint-tree -g
+
+## Usage
+
+    jsonlint-tree myfolder
+
+## Changelog
+
+* 1.0.0 
+    * Initial release :tada:
+
+## License
+
+[ISC License](http://opensource.org/licenses/ISC)
---
+++ index.js
@@ -0,0 +1,29 @@
+#!/usr/local/bin/node
+var fs          = require('fs')
+var readdir     = require('recursive-readdir')
+var jsonlint    = require('jsonlint')
+var colors      = require('colors')
+var replaceall  = require('replaceall')
+var argv        = require('minimist')(process.argv.slice(2))
+
+const check = String.fromCharCode(0x2713).green
+const cross = String.fromCharCode(0x2717).red
+
+var folder = argv._[0].startsWith('/') ? argv._[0] : `${process.cwd()}/${argv._[0]}`
+
+readdir(folder, (err, files) => {
+    files.forEach(file => {
+        if (file.endsWith('.json')) {
+            fs.readFile(file, 'utf8', (err, data) => {
+                if (err) throw err
+                try {
+                    jsonlint.parse(data)
+                    console.log(check, file)
+                }
+                catch (e) {
+                    console.log(cross, file, `\n\t${replaceall('\n', '\n\t', e.toString())}`)
+                }
+            })
+        }        
+    })
+})
