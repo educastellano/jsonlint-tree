5814ecbbe1b34fbb390fb311d28ffb5b01fbbbd4
Eduard Castellano <educastellano08@gmail.com>
2026-07-22 11:19:56 +0200

    Fix summary not showing when non json files are present

--- index.js
+++ index.js
@@ -23,30 +23,28 @@ fs.readdir(folder, { recursive: true, withFileTypes: true }, (err, entries) => {
   if (err) throw err
 
   const files = entries
-    .filter(entry => entry.isFile())
+    .filter(entry => entry.isFile() && entry.name.endsWith('.json'))
     .map(entry => path.join(entry.parentPath ?? entry.path, entry.name))
 
   for (const file of files) {
-    if (file.endsWith('.json')) {
-      fs.readFile(file, 'utf8', (err, data) => {
-        if (err) throw err
-        try {
-          jsonlint.parse(data)
-          console.log(check, file)
-          passed++
-        }
-        catch (e) {
-          failed++
-          console.error(cross, file, `\n\t${e.toString().split('\n').join('\n\t')}`)
-        }
-        if (passed+failed === files.length) {
-          console.log('\n')
-          console.log('  ', check, passed, 'valid files')
-          console.log('  ', cross, failed, 'invalid files')
-          console.log('\n')
-          process.exit(failed)
-        }
-      })
-    }
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) throw err
+      try {
+        jsonlint.parse(data)
+        console.log(check, file)
+        passed++
+      }
+      catch (e) {
+        failed++
+        console.error(cross, file, `\n\t${e.toString().split('\n').join('\n\t')}`)
+      }
+      if (passed+failed === files.length) {
+        console.log('\n')
+        console.log('  ', check, passed, 'valid files')
+        console.log('  ', cross, failed, 'invalid files')
+        console.log('\n')
+        process.exit(failed)
+      }
+    })
   }
 })
